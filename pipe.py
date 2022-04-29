4bd8010c79dcb62c63648a39bf57c3b2b85413c8
Matheus Junqueira <76868677+matheusjunqueiradasilva@users.noreply.github.com>
2022-04-29 17:31:49 -0300

    v1.0

--- pipe.py
+++ pipe.py
@@ -4,12 +4,13 @@ import pandas as pd
 import json
 import psycopg2
 import requests
+import pprint
 
 class APIMiddleware:
-    def _init_ (self, endpoint):
+    def __init__(self, endpoint):
         # Inicializa a engine do postgress e seta o endpoint
-        self.engine = create_engine("postgresql://postgres:god123@localhost:5433/postgres")
         self.endpoint = endpoint
+        self.engine = create_engine("postgresql://postgres:2ky7wM&duA4@localhost:5433/CostumeSucess")
 
     def save(self, nome_tabela="export_default"):
         # Realizar requisicao para o endpoint
@@ -20,7 +21,7 @@ class APIMiddleware:
 
         # Pega o objeto Data
         request_data = _resultado.get("data")
-        print(_resultado)
+        pprint.pprint(request_data)
 
         # Salvo o objeto data dentro do dataframe
         request_data_frame = pd.DataFrame(request_data)
@@ -32,11 +33,11 @@ class APIMiddleware:
         
 
 
-pegar_dados_organizations = APIMiddleware("https://csp.pipedrive.com/api/v1/organizations:()?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
-pegar_dados_organizations.save("salvando_organizations")
+#pegar_dados_organizations =APIMiddleware("https://csp.pipedrive.com/api/v1/organizations(name)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
+#pegar_dados_organizations.save("salvando_organizations")
 
-pegar_dados_dels= APIMiddleware("https://csp.pipedrive.com/api/v1/deals:()?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
-pegar_dados_dels.save("salvando dels")
+#pegar_dados_dels= APIMiddleware("https://csp.pipedrive.com/api/v1/deals:(org_name,title,status,value)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
+#pegar_dados_dels.save("salvando dels")
 
-pegar_dados_person = APIMiddleware("'https://csp.pipedrive.com/api/v1/persons:()?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
+pegar_dados_person = APIMiddleware('https://csp.pipedrive.com/api/v1/persons:(name,org_name,owner_name,email,phone)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c')
 pegar_dados_person.save("salvando person")
