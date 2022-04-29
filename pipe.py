762072d8909cffcd9c7c0cb3a888fc2bb272c5d3
Matheus Junqueira <76868677+matheusjunqueiradasilva@users.noreply.github.com>
2022-04-29 17:33:29 -0300

    v1.1

--- pipe.py
+++ pipe.py
@@ -10,7 +10,7 @@ class APIMiddleware:
     def __init__(self, endpoint):
         # Inicializa a engine do postgress e seta o endpoint
         self.endpoint = endpoint
-        self.engine = create_engine("postgresql://postgres:2ky7wM&duA4@localhost:5433/CostumeSucess")
+        self.engine = create_engine("")
 
     def save(self, nome_tabela="export_default"):
         # Realizar requisicao para o endpoint
@@ -33,11 +33,11 @@ class APIMiddleware:
         
 
 
-#pegar_dados_organizations =APIMiddleware("https://csp.pipedrive.com/api/v1/organizations(name)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
+#pegar_dados_organizations =APIMiddleware("")
 #pegar_dados_organizations.save("salvando_organizations")
 
-#pegar_dados_dels= APIMiddleware("https://csp.pipedrive.com/api/v1/deals:(org_name,title,status,value)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c")
+#pegar_dados_dels= APIMiddleware("")
 #pegar_dados_dels.save("salvando dels")
 
-pegar_dados_person = APIMiddleware('https://csp.pipedrive.com/api/v1/persons:(name,org_name,owner_name,email,phone)?api_token=844b0375fc6a4810c1ee9561204bee2e0d752e8c')
+pegar_dados_person = APIMiddleware('')
 pegar_dados_person.save("salvando person")
