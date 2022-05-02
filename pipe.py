907f2afb97a1ad8b6d822d11feed5286939b6c33
Matheus Junqueira <76868677+matheusjunqueiradasilva@users.noreply.github.com>
2022-05-02 15:47:52 -0300

    Update pipe.py
    atualizacao 1.2

--- pipe.py
+++ pipe.py
@@ -5,6 +5,7 @@ import json
 import psycopg2
 import requests
 import pprint
+from flatten_json import flatten
 
 class APIMiddleware:
     def __init__(self, endpoint):
@@ -18,10 +19,9 @@ class APIMiddleware:
         _resultado = _requisicao.json()
     
         print('requisição feita com sucesso')
-
+       
         # Pega o objeto Data
         request_data = _resultado.get("data")
-        pprint.pprint(request_data)
 
         # Salvo o objeto data dentro do dataframe
         request_data_frame = pd.DataFrame(request_data)
@@ -30,14 +30,29 @@ class APIMiddleware:
         request_data_frame.to_sql(nome_tabela, self.engine, if_exists='replace')
         
         print("tabela", nome_tabela, "salva com sucesso")
-        
+
+    def formata(self,nome_tabela = "export_default"):
+        _requisicao = requests.get(self.endpoint)
+        _resultado = _requisicao.json()
+        _resultado = _resultado.get("data")
+
+# formata o json e envia pra um dataframe
+        dict_flatten = (flatten(d) for d in _resultado)
+
+        df = pd.DataFrame(dict_flatten)
+# Salva o dataframe dentro do SQL
+        df.to_sql(nome_tabela, self.engine, if_exists='replace')
 
 
-#pegar_dados_organizations =APIMiddleware("")
-#pegar_dados_organizations.save("salvando_organizations")
+    
 
-#pegar_dados_dels= APIMiddleware("")
-#pegar_dados_dels.save("salvando dels")
+
+pegar_dados_organizations =APIMiddleware("")
+pegar_dados_organizations.save("organizations")
+
+pegar_dados_dels= APIMiddleware("")
+pegar_dados_dels.save("dels")
 
 pegar_dados_person = APIMiddleware('')
-pegar_dados_person.save("salvando person")
+pegar_dados_person.formata("person")
+
